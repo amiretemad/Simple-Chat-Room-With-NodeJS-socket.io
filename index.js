c42e20d2ee3966d66fafcadac6aa7f9718c8e4f2
Amir <amiretemad2000@gmail.com>
2017-04-29 11:31:05 +0430

    Change Interface of chatroom and Display Date of sent messages

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Message count
+var messageCount = 0;
+
 io.on('connection', function (socket) {
     // Display Log in Terminal
     console.log(" A User connected ! ");
     // Chat Message Event
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        var today = new Date();
+        var data =
+        {
+            'message': msg,
+            'messageCount': messageCount++,
+            'date': today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        }
+        io.emit('chat message', data);
     });
     // Disconnect Event
     socket.on('disconnect', function () {
